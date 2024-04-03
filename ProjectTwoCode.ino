4f899afab7fb86f5296a141a520d329b5318dc47
Allan-Quezada <70115377+Allan-Quezada@users.noreply.github.com>
2024-04-03 14:43:29 -0500

    Add files via upload

---
+++ ProjectTwoCode.ino
@@ -0,0 +1,28 @@
+#include <Servo.h>
+const int switchPin = 8;
+const int switchPin02 = 7
+int servoAngle = 0;
+float servoSpeed = 0.5;
+
+void setup() {
+  pinMode(switchPin, INPUT);
+  myservo.attach(9);
+  myservo.write(servoAngle);
+
+
+}
+
+void loop() {
+  if (digitalRead(switchPin) == HIGH) {
+    servoAngle += servoSpeed;
+  } else {
+    servoAngle = 0;
+  }
+  myservo.write(servoAngle);
+  
+  if (digitialRead(switchPin02) == HIGH) {
+    servoAngle.write(180);
+  } else {
+    servoAngle = 0;
+  }
+}
